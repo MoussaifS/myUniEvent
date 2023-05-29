1e8b5ac0ff1c0a065d38a8e5b63bf57655e872e1
MoussaifS <skmsa299@gmail.com>
2023-05-29 11:22:16 -0400

    project init

---
+++ unievent-portal/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button';
+import Form from './components/Form';
+
+function App() {
+
+  return (
+    <div>
+    <Form/>
+    </div>
+  )
+}
+
+export default App
