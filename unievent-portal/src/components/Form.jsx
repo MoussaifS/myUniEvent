99839f7a6d4d54e689bfcd4b94aec73ab4e4eca1
MoussaifS <skmsa299@gmail.com>
2023-05-29 14:42:48 -0400

    form depencey && change react version

--- unievent-portal/src/components/Form.jsx
+++ unievent-portal/src/components/Form.jsx
@@ -1,11 +1,41 @@
-import { useState } from "react";
+import Select from "react-select";
+import { useForm, Controller } from "react-hook-form";
+import Input from '@mui/material/Input';
 
 const Form = () => {
-    return (
-        <div>
-        
-        </div>
-    )
-}
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: '',
+      select: {}
+    }
+  });
+  const onSubmit = data => console.log(data);
+
+  return (
+    <div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="firstName"
+        control={control}
+        render={({ field }) => <Input {...field} />}
+      />
+      <Controller
+        name="select"
+        control={control}
+        render={({ field }) => <Select 
+          {...field} 
+          options={[
+            { value: "chocolate", label: "Chocolate" },
+            { value: "strawberry", label: "Strawberry" },
+            { value: "vanilla", label: "Vanilla" }
+          ]} 
+        />}
+      />
+      <input type="submit" />
+    </form>
+    </div>
+    
+  );
+};
 
 export default Form
