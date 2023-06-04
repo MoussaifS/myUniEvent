79bcd7c0fcd670f22c60974a4f8a153cdb36febf
MoussaifS <skmsa299@gmail.com>
2023-06-04 12:31:21 -0400

    chatgpt for landing

--- unievent-portal/src/pages/Landing.jsx
+++ unievent-portal/src/pages/Landing.jsx
@@ -1,34 +1,62 @@
 import * as React from "react";
 import pic from "../assets/image1-removebg.png";
-import { Container, Typography, Grid, Button } from "@mui/material";
+import { Box, Container, Typography, Grid, Button } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
 import Login from "../components/Login";
 import SignUp from "../components/SignUp";
 const Landing = () => {
   return (
-    <Grid container spacing={8}>
+    <Container maxWidth="xl">
+      <Grid container spacing={8}>
       <Grid item xs={6}>
-        <Container maxWidth="sm" id="landing-form">
-          <Login />
-          <SignUp />
-        </Container>
-      </Grid>
-      <Grid item xs={6}>
-        <Container maxWidth="sm" id="landing-prom">
+      <Container maxWidth="sm" id="landing-prom">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+          }}
+        >
           <Typography variant="h2" gutterBottom>
             Uni-Event
           </Typography>
-
-          <Typography variant="subtitle1" gutterBottom>
-          subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-          blanditiis tenetur
-        </Typography>
-          <img src={pic} />
-        </Container>
-      </Grid>
+          <Typography variant="h6" gutterBottom>
+            Student-Made For Student Use
+          </Typography>
+          <img
+            src={pic}
+            alt="Promotional Image"
+            style={{ maxWidth: '100%', height: 'auto' }}
+          />
+        </Box>
+      </Container>
     </Grid>
+
+
+
+        <Grid item xs={6}>
+          <Container maxWidth="sm" id="landing-form">
+            <Box
+              sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'row',
+              }}
+            >
+              <SignUp />
+              
+            </Box>
+          </Container>
+        </Grid>
+
+      </Grid>
+    </Container>
   );
 };
+
 
 export default Landing;
