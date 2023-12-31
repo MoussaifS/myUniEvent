467457589ca8c58ee800ca84ab99c87df0deac6d
MoussaifS <skmsa299@gmail.com>
2023-12-31 23:54:20 +0800

    filter ui

--- unievent-portal/src/components/Filter.jsx
+++ unievent-portal/src/components/Filter.jsx
@@ -4,46 +4,77 @@ import "@material/web/chips/assist-chip.js";
 import "@material/web/chips/suggestion-chip.js";
 import "@material/web/chips/filter-chip.js";
 import "@material/web/switch/switch.js";
+
+import { useState } from "react";
+
+import { Accordion, AccordionDetails, AccordionSummary,  Typography  } from "@mui/material";
+
+
+
 import { eventTagList } from "../lists/EventTagsList";
 const Filter = (props) => {
 
   
   //sudo code 
-  const HandleTag(e)=>{
+  const handleTag = (e) => {
+    console.log(e.target.label)
     // 1- pass to the query
     // 2- navigate to the event page with the filter 
     // 3- if query empty display all 
     // 4- remove the date filters for now 
   }
 
+  const [expanded, setExpanded] = useState(false);
+
+
+
+const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
 
   return (
     <div id="duck">
-      <span id="filter-secondary-span">Categories:</span>
-      <div id="card-horzintal-scroll">
-        <div>
+
+     <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            Filters:
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
+
           <md-filter-chip
             id="tags-horzintal"
-            label="This Week"
+            
+            label="tag"
+            elevated
+            onClick={(e)=>{handleTag(e)}}
           ></md-filter-chip>
-          <md-filter-chip
-            id="tags-horzintal"
-            label="This Month"
-          ></md-filter-chip>
-          <md-filter-chip
-            id="tags-horzintal"
-            label="All Events"
-          ></md-filter-chip>
-        </div>
+
+          
+        </AccordionSummary>
+        <AccordionDetails>
+        <div id="card-horzintal-scroll">
         {eventTagList.map((eventTag, index) => (
           <md-filter-chip
             id="tags-horzintal"
             key={index}
             label={eventTag.tag}
             elevated
+            onClick={(e)=>{handleTag(e)}}
           ></md-filter-chip>
         ))}
       </div>
+        </AccordionDetails>
+      </Accordion>
+      
+
+
+
+      
     </div>
   );
 };
